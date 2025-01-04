a48f3f9684c94b04da5ac8c444273616e4d50723
Vladimir Obidchuk <71866954+VladimirObidchuk@users.noreply.github.com>
2025-01-04 19:46:43 +0200

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,12 @@
+function makeArray(array1,array2,number){
+    const words = array1.concat(array2);
+    return words.length === number ? words : words.slice(0, number);
+}
+
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
